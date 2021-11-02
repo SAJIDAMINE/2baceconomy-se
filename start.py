e53b1310ce6221bf6aa513b94b39b1b81a9423ce
SAJIDAMINE <86064325+SAJIDAMINE@users.noreply.github.com>
2021-11-02 08:19:24 +0000

    start.py

---
+++ start.py
@@ -0,0 +1,49 @@
+print ("Enter your resault in :")
+maths = float(input (" Maths :"))
+eco = float(input (" Eco :"))
+compta = float(input (" Compta :"))
+orga = float(input (" Orga :"))
+english = float(input (" English :"))
+fr = float(input (" Fr :"))
+info = float(input (" Info :"))
+sport = float(input (" Sport :"))
+droit = float(input (" Droit :"))
+history = float(input (" History :"))
+islamic = float(input (" Islamic :"))
+arabic = float(input (" Arabic :"))
+philo = float(input (" Philo :"))
+
+eco = eco *6
+maths = maths * 4
+compta = compta * 4
+orga = orga *3
+english = english *2
+fr = fr *3
+info = info *4
+sport = sport *4
+droit = droit *4
+history = history *3
+islamic = islamic *2
+arabic = arabic *2
+philo = philo*2
+
+resault = eco + maths + compta + orga + english + fr + info + sport + droit + history + islamic + arabic + philo
+resault = resault / 44
+print ("your resault is :",resault)
+if resault >= 10 :
+	print ("congratulations (~: !!")
+else : 
+    print (" your future not related by your school !! and stop kidding me  (;")
+
+jihawi = float(input("  regional :"))
+morakaba = float(input ("  controles continues :"))
+normalise1 = float(input ("normalisé 1 (الامتحان الموحد للدورة الاولى) :"))
+watani = float(input ("  national :"))
+jihawi = jihawi * 25
+morakaba = morakaba * 12.5
+normalise1 = normalise1 * 12.5
+watani = watani * 50
+demi_resault = jihawi + morakaba + normalise1 + watani 
+final_resault = demi_resault / 100
+print (final_resault)
+print ("#by SAJID")
